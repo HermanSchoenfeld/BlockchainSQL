3144fdf5d5d76cad4fd87ab09b0fa47b784290f8
Herman Schoenfeld <herman@sphere10.com>
2022-09-27 19:17:55 +1000

    Cleanup

--- BlockchainSQL.Server/InstallWizard/InstallWizard.cs
+++ BlockchainSQL.Server/InstallWizard/InstallWizard.cs
@@ -1,51 +1,42 @@
 ﻿using Hydrogen;
-using Hydrogen.Data;
 using Hydrogen.Windows.Forms;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Reflection;
-using System.Text;
+
 using System.Threading.Tasks;
-using BlockchainSQL.Processing;
-using NHibernate.Proxy;
-using Hydrogen.Application;
 
-namespace BlockchainSQL.Server {
-	public class InstallWizard : WizardBase<InstallWizardModel> {
+namespace BlockchainSQL.Server;
 
-		public InstallWizard() 
-			: base("Intall BlockchainSQL Server Service", InstallWizardModel.Default, "Install") {
-		}
+public class InstallWizard : WizardBase<InstallWizardModel> {
 
-		public async Task Validate() {
-			// Rely on screen validations
-		}
-
-
-		protected override async Task<Result> Finish() {
-			try {
-				await ServiceManager.LaunchInstallServiceProcess(Model.ServiceDirectory,
-					Model.StartAfterInstall,
-					Model.ServiceDatabaseSettings,
-					Model.NodeSettings,
-					Model.ScannerSettings,
-					Model.WebSettings);
-			} catch (Exception error) {
-				return Result.Error("Failed to install Service Process.", error.ToDisplayString());
-			}
-			return Result.Valid;
-		}
-
-		protected override IEnumerable<WizardScreen<InstallWizardModel>> ConstructScreens() {
-			yield return new InstallationDirectoryScreen();
-			yield return new ServiceDatabaseSettingsScreen();
-			yield return new ServiceNodeSettingsScreen();
-			yield return new ServiceScannerSettingsScreen();
-			yield return new WebSettingsScreen();
-		}
-
+	public InstallWizard()
+		: base("Install BlockchainSQL Server Service", InstallWizardModel.Default, "Install") {
 	}
+
+	public async Task Validate() {
+		// Rely on screen validations
+	}
+
+	protected override async Task<Result> Finish() {
+		try {
+			await ServiceManager.LaunchInstallServiceProcess(Model.ServiceDirectory,
+				Model.StartAfterInstall,
+				Model.ServiceDatabaseSettings,
+				Model.NodeSettings,
+				Model.ScannerSettings,
+				Model.WebSettings);
+		} catch (Exception error) {
+			return Result.Error("Failed to install Service Process.", error.ToDisplayString());
+		}
+		return Result.Valid;
+	}
+
+	protected override IEnumerable<WizardScreen<InstallWizardModel>> ConstructScreens() {
+		yield return new InstallationDirectoryScreen();
+		yield return new ServiceDatabaseSettingsScreen();
+		yield return new ServiceNodeSettingsScreen();
+		yield return new ServiceScannerSettingsScreen();
+		yield return new WebSettingsScreen();
+	}
+
 }
